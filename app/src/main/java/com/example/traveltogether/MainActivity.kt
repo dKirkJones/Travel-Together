8122e372e598e0e810d38ade375baeb388debe7d
Darrin K Jones <djmakesapps@gmail.com>
2023-02-24 13:54:46 -0600

    add back navigation to screens

--- app/src/main/java/com/example/traveltogether/MainActivity.kt
+++ app/src/main/java/com/example/traveltogether/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.traveltogether.data.Attraction
 import com.example.traveltogether.data.AttractionsResponse
 import com.squareup.moshi.JsonAdapter
@@ -14,6 +17,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private  val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     val attractionsList: List<Attraction> by lazy {
@@ -25,6 +29,13 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) ||super.onSupportNavigateUp()
     }
 
     private fun parseAttractions(): List<Attraction> {
